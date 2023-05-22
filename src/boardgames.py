6560ef66926d4a9baccdd6da994c3664ac968d99
nandobfer <nandobfer@yahoo.com.br>
2023-05-22 08:47:45 -0300

    feat: added footer to prduct page

--- src/boardgames.py
+++ src/boardgames.py
@@ -19,7 +19,6 @@ def getBoardgames():
     return alternatives
 
 def getCriteria():
-
     for index, criterion in enumerate(criteria):
         cri = Criterion(index+1, criterion['name'], CriterionType.COST if criterion['type'] == 'COST' else CriterionType.BENEFIT)
         _criteria.append(cri)
@@ -31,17 +30,25 @@ def setCriteria(dm):
         rating = criteria[index]['weight'][dm.id-1]
         dm.add_new_criterion_judgment(CriterionJudgment(criterion.id, rating[0], rating[1]))
 
-def judge(criteria, dm, alternatives):
+def judge(criteria: list[Criterion], dm, alternatives):
+    print(f'dm{dm.id}:')
     for alternative in alternatives:
         game = boardgames[alternative.id-1]
+        print(game['name'])
 
         for index, data in enumerate(game['data']):
             rating = judgeData(game['data'][data], data, index)
+            # print(f"data_name: {data}, data_value: {game['data'][data]}, rating: {rating}")
             criteria[index].add_new_alternative_judgment(AlternativeJudgment(alternative.id, dm.id, rating, rating))
+            print({data: [criteria[index].alternative_judgments[-1].min_value, criteria[index].alternative_judgments[-1].max_value]})
 
         for index, judgement in enumerate(game['judgement']):
             rating = game['judgement'][judgement][dm.id-1]
             criteria[index+2].add_new_alternative_judgment(AlternativeJudgment(alternative.id, dm.id, rating[0], rating[1]))
+            print({judgement: [criteria[index].alternative_judgments[-1].min_value, criteria[index].alternative_judgments[-1].max_value]})
+            print(f'should be {rating}')
+        
+        print('')
 
 def judgeData(value, data, benefit):
     min_value = min(boardgames, key=lambda x:x['data'][data])['data'][data]
@@ -55,3 +62,4 @@ def judgeData(value, data, benefit):
     
 
     return rating
+
